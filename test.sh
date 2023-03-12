a70b23cfd63bb9f78b92e7b4cd1d7a90d6e71bbb
Dominique Martinet <asmadeus@codewreck.org>
2023-03-12 13:56:35 +0900

    test: move -n into a separate helper
    we'll want to add a test without -n next...

--- test.sh
+++ test.sh
@@ -13,7 +13,7 @@ run() {
 	done
 	shift
 	python3 ./shortcuts.py --no-sleep -e /dev/stdout "${first[@]}" \
-		| python3 ./shortcuts.py --no-sleep -e /dev/stdin -n "$@" 2>&1
+		| python3 ./shortcuts.py --no-sleep -e /dev/stdin "$@"
 }
 
 run_replay() {
@@ -29,29 +29,34 @@ run_generated() {
 	run --replay-action "$name" -- "$@"
 }
 
+rgrep() {
+	local key="$1"
+	shift
+	"$@" -vn 2>&1 >/dev/null | grep -q "$key"
+}
+
 FAILED=0
 
 run_replay tests/monkey-touch.record \
 	|| error "monkey-touch crashed"
-run_replay tests/double-tap-left.record -v | grep -q left \
+rgrep left run_replay tests/double-tap-left.record \
 	|| error "left didn't act on left"
-run_replay tests/double-tap-right.record -v | grep -q right \
+rgrep right run_replay tests/double-tap-right.record \
 	|| error "right didn't act on right"
-run_replay tests/double-tap-top.record -v | grep -q top \
+rgrep top run_replay tests/double-tap-top.record \
 	|| error "top didn't act on top"
-run_replay tests/double-tap-slow-top-no-event.record -v | grep -q top \
+rgrep top run_replay tests/double-tap-slow-top-no-event.record \
 	&& error "shouldn't trigger top on slow taps"
 
-run_generated double_tap_left -v | grep -q left \
+rgrep left run_generated double_tap_left \
 	|| error "left didn't act on synthetic left"
-run_generated double_tap_right -v | grep -q right \
+rgrep right run_generated double_tap_right \
 	|| error "right didn't act on synthetic right"
-run_generated double_tap_top -v | grep -q top \
+rgrep top run_generated double_tap_top \
 	|| error "top didn't act on synthetic top"
 
 run_generated double_tap_left --record \
-		| run --replay -- -v \
-		| grep -q left \
+		| rgrep left run --replay --\
 	|| error "replay of recording of synthetic left didn't recognize left"
 
 [ "$FAILED" = 0 ]
